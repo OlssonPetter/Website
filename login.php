7c88859de9a35cff5b6efa6416aa47cd6a1abeb0
OlssonPetter <petter1olsson@gmail.com>
2017-05-18 14:36:54 +0200

    Login
    Tragiskt försök till att fixa härlig design till login.php. Blev tyvärr
    lite 80-tal över det hela. Skulle dock säga att det är ganska charmigt!

--- login.php
+++ login.php
@@ -6,25 +6,30 @@
     <title>ACTIV8</title>
     </head>
     <body>
-            <div class="form-box">
-    <div class="head">Welcome Back!</div>
-    <form action="#" id="login-form">
-        <div class="form-group">
-          <label class="label-control">
-            <span class="label-text">Email</span>
-          </label>
-          <input type="email" name="email" class="form-control" />
+        <form id="formLogin" action="login.php" method="post">
+        
+        <div>
+        <p class = "Formulär"> 
+        <label for="EpostLogin">E-post</label>
+        </p>
+        
+        <p>
+        <input type="text" id="epostlogin" name = "epostlogin" placeholder="Vänligen skriv e-mail här...">
+        </p>
+        
+        <p class = "Formulär">
+        <label for="LösenordLogin">Lösenord</label>
+        </p>
+        
+        <p>
+        <input type="password" style="-webkit-text-security: square;" id="passwordlogin" name = "passwordlogin" placeholder="Vänligen skriv lösenord här...">
+        </p>
+        
+        <p class = "Formulär"><input class ="button" id ="Login" type="submit" value="Logga in!"></p>
         </div>
-        <div class="form-group">
-          <label class="label-control">
-            <span class="label-text">Password</span>
-          </label>
-          <input type="password" name="password" class="form-control" />
-        </div>
-        <input type="submit" value="Login" class="btn" />
-        <p class="text-p">Don't have an account? <a href="#">Sign up</a> </p>
     </form>
-  </div>
+
+  
     </body>
     <link rel="stylesheet" href="projekt.css"/>
     <meta charset="utf-8">
