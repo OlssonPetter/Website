d441055ce1dfddc631e3b3a0cd9a770543cc4c09
OlssonPetter <petter1olsson@gmail.com>
2017-05-18 15:00:28 +0200

    Anslutning.php dab

---
+++ anslutning.php
@@ -0,0 +1,14 @@
+<?php
+
+    $localhost = "localhost";
+    $user = "root";
+    $password = "root";
+    $database = "project";
+ 
+    $conn = new mysqli('localhost', 'root', 'root', 'project');
+
+    if ($conn->connect_error) 
+    {
+        die("Connection failed: " . $conn->connect_error);    
+    }
+?>
