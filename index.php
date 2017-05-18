4e47ffcc1fee8b5f3846c857bf7f555d7bcc6285
OlssonPetter <petter1olsson@gmail.com>
2017-05-18 13:21:57 +0200

    skev startsida felåt

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
     <meta charset="utf-8">
     </head>
     <body>
-    <h1 class ="Startsida"id="Först">WELCOME TO!</h1>
-    <h2 class ="Startsida"id="Mitten">ACTIV8!</h2>
-    <h3 class ="Startstida"id="Sist">THE ACTIVE COMMUNITY!</h3>
+    <h1 class ="Startsida"id="Först">WELCOME TO</h1>
+    <h2 class ="Startsida"id="Mitten">ACTIV8</h2>
+    <h3 class ="Startstida"id="Sist">THE ACTIVE COMMUNITY.</h3>
     
     <a href="login.php" class="buttonstart1" type="button" >Logga in!</a>
     <a href="register.php" class="buttonstart2" type="button" >Registera dig!</a>  
