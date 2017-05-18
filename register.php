9f7c0bb70cdbcaab5fc78d617a494ba49c1f4f35
oskar landen <oskar landen>
2017-05-18 14:37:08 +0200

    Skrev html-grunden för register.php
    Skrev kod för registreringsform och header.

--- register.php
+++ register.php
@@ -6,10 +6,24 @@
     <title>ACTIV8</title>
     </head>
     <body>
-    <p>Register</p>
+
+      <div class"header">
+        <a="index.ph">BasicHeader</a>
+      </div>
+
+      <h1>Register below</h1>
+      <span> or <a href="login.php">login here</a></span>
+
+      <form action="register.php" method="POST" name="myRegForm" onsubmit="return validateRegister()">
+
+        <input type="text" placeholder="Please, enter your email" name="email">
+        <input type="text" placeholder="and password" name="password">
+        <input type="text" placeholder="confirm password" name="confirm_password">
+        <input type="text" placeholder="Please, add some information about yourself" name="info">
+        <input type="text" placeholder="Please, enter your sex" name="sex">
+        <input type="text" placeholder="Please, enter your age" name="age">
+
+      </form>
+
     </body>
-
-
-
-
 </html>
