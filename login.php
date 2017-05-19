ccd714ff36b7ad671e6eb761999dc517641b5cbf
Spikeroo <erikliden@student95-181.eduroam.uu.se>
2017-05-19 10:28:46 +0200

    login
    Kollar om username finns i databasen, utan sessions

--- login.php
+++ login.php
@@ -1,4 +1,23 @@
 <?php
+require'anslutning.php';
+$user =mysqli_real_escape_string($conn,$_POST['email']);
+   $password =mysqli_real_escape_string($conn,$_POST['password']);
+
+   $records= $con->query("SELECT * FROM User WHERE email ='$user'")
+   or die("Failed to match query database");
+   $results=$records->fetch_assoc();
+   $password=$password.$results['salt'];
+   $password=sha1($password);
+
+   if(count($results)>0&& $password==$results['password'])
+   {
+     echo("its a match");
+
+   }
+   else {
+    die("did not find match in database");
+   }
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,14 +26,14 @@
     </head>
     <body>
 <form>
-  <input placeholder="Email">
-  <input type="password" placeholder="Password">
+  <input placeholder="Email" name= "email">
+  <input type="password" placeholder="Password" name="password">
   <button>Login</button>
   <p>Or register <a href="register.php">here</a>.</p>
 </form>
 
 
-  
+
     </body>
     <link rel="stylesheet" href="projekt.css"/>
     <meta charset="utf-8">
