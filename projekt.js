3bb54d19dc0939eb7afca468f1f01cc8ab3d531f
OlssonPetter <petter1olsson@gmail.com>
2017-05-22 14:09:55 +0200

    Jävla massa ändringar. Läs desciption!
    har skapat startpage.php, ska väl fungera som något typ av feed med en
    dropdown menu ovanför. Typ som vi vill att startsidan ska se ut! Den går
    inte att komma åt via någon knapp, skriv in
    http://localhost/Website/startpage.php istället! JS är ganska konstig.
    Men har tagit bort mycket onödigt och skrev en del kommentarer. Är
    startpage.php överkurs så kan vi skita i det och göra en egen från noll!

--- projekt.js
+++ projekt.js
@@ -1,1 +1,35 @@
+$(function() {
+  $(".dark").click(function() {
+    $("#feed").addClass('dark');
+  });
+  $(".light").click(function() {
+    $("#feed").removeClass('dark');
+  });
+  var url = 'http://codepen.io/jobs/feed/'; ///Här måste det ändras 
+    var rubrik = "";
+    var innehåll = "";
+    var länktillevent = "";
+    var skapare = "";
+    var länktilldelta = "";
+    
+  
+  $.ajax({
+    type: "GET",
+    url: document.location.protocol + '//api.rss2json.com/v1/api.json?rss_url=' + encodeURIComponent(url),
+    dataType: 'json',
+    error: function() {
+      $("#feed").after("<center>Unable to load feed, Incorrect path or invalid feed</center>");
+    },
+    success: function(xml) {
+      values = xml.items;
 
+      for (var i = 0, j = values.length; i < j; i++) {
+        // console.log(values[i]);
+        ////// I values vill vi lagra alla värden som ska synas i startsidans feed. T.ex. namn på event, info, osv.   
+        
+          //////Detta är informationen som visas i startpage.php. Borde gå att ta från en användarinput istället. Alltså att lagra alla event i variabeln values och loopa igenom tabellen som lagrar alla event.  
+        $("#feed").append("<li><a href='" + länktillevent.link + "' target='_blank'>" + rubrik.title + "</a><br><i>" + skapare.author + "</i><a href='" + länktilldelta.link + "' class='apply'  target='_blank'>APPLY</a><div>" + innehåll.content + "</div></li>");
+      }
+    }
+  });
+});
---
+++ startpage.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+    <div class="container">
+  <a href="#home">Profile</a>
+  <a href="#news">Friends</a>
+  <div class="dropdown">
+    <button class="dropbtn">Events</button>
+    <div class="dropdown-content">
+      <a href="#">Create event</a>
+      <a href="#">Find event</a>
+    </div>
+  </div> 
+  <a href="#Booking"></a>
+</div>
+    </head>
+<body>
+    
+<div class="opt"></div>
+<ul id="feed"></ul>
+<div 
+     style="font-size: 12px;  
+        text-align: center;  
+        color: #666;  
+        background:rgba(0,0,0,0.8);    
+        max-width: 100%; width: 100vw;   
+        opacity: 0.9;
+        padding:5px 0;">
+</div>
+</body>
+    <link rel="stylesheet" href="projekt.css"/>
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="projekt.js" ></script>
+
+</html>
