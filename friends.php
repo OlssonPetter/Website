02095dd9781511cd848ef7dfe4fe539eeffda164
Spikeroo <erik.liden@hotmail.co.uk>
2017-05-24 16:13:59 +0200

    Skapat friendssida

---
+++ friends.php
@@ -0,0 +1,11 @@
+!<!DOCTYPE html>
+<html>
+<head>
+  <title> ACTIV8 </title>
+    <meta charset="utf-8">
+    <h1> Here are your friends!<h1>
+</head>
+<body>
+</body>
+  <link rel="stylesheet" href="projekt.css"/>
+</html>
