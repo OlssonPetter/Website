10185d334b446323521835841d6f32f9fded3143
OlssonPetter <petter1olsson@gmail.com>
2017-05-24 14:50:13 +0200

    Booking.php

---
+++ booking.php
@@ -0,0 +1,49 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>ACTIV8</title>
+        <div class="container">
+  <a href="profile.php">Profile</a>
+  <a href="friends.php">Friends</a>
+<a href="event.php">Events</a>
+  <a href="booking.php">Booking</a>
+</div>
+    </head>
+    <body>
+        <h2>Personal trainer</h2>
+        <div class="booking">
+        <p>Press here to make booking at Actic</p>
+        <a href="http://www.actic.se/traning/personlig-traning/" class="button" type="button" >Visit website!</a>
+        </div>
+        
+        <div class="booking">
+        <p>Press here to make booking at Friskis&Svettis</p>
+        <a href="http://www.uppsala.friskissvettis.se/" class="button" type="button" >Visit website!</a>
+        </div>
+        
+        <div class="booking">
+        <p>Press here to make booking at Campus1477</p>
+        <a href="http://www.campus1477.se/sv/tjanster/gym-personlig-traning/personlig-traning/" class="button" type="button" >Visit website!</a>
+        </div>
+        
+        <h2>Training facilities</h2>
+        <div class="booking">
+        <p>Press here to make booking at Uppsala tennisklubb! </p>
+        <a href="http://www.utk.se/" class="button" type="button" >Visit website!</a>
+        </div>
+        <div class="booking">
+        <p>Press here to make booking at Golf.se!</p>
+        <a href="http://www.golf.se/" class="button" type="button" >Visit website!</a>
+        </div>
+        
+        
+    
+    </body>
+    <link rel="stylesheet" href="projekt.css"/>
+    <meta charset="utf-8">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="projekt.js" ></script>
+
+</html>
