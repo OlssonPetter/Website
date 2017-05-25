611babbae49670107c739104aef46e4fff1509d3
OlssonPetter <petter1olsson@gmail.com>
2017-05-25 21:59:02 +0200

    Checkbox för terms + kontroll i JS att det måste vara ifylld

--- projekt.js
+++ projekt.js
@@ -1,24 +1,14 @@
-$(function() {
-  $(".dark").click(function() {
-    $("#feed").addClass('dark');
-  });
-  $(".light").click(function() {
-    $("#feed").removeClass('dark');
-  });
- 
-    var rubrik = "NamnPåEvent";
-    var innehåll = "InfoOmEvent";
-    var länktillevent = "";
-    var skapare = "Creater";
-    var länktilldelta = "";
-        // console.log(values[i]);
-        ////// I values vill vi lagra alla värden som ska synas i startsidans feed. T.ex. namn på event, info, osv.   
-        
-          //////Detta är informationen som visas i startpage.php. Borde gå att ta från en användarinput istället. Alltså att lagra alla event i variabeln values och loopa igenom tabellen som lagrar alla event.  
-        $("#feed").append("<li><a href='" + länktillevent + "' target='_blank'>" + rubrik + "</a><br><i>" + skapare + "</i><a href='" + länktilldelta + "' class='apply'  target='_blank'>JOIN!</a><div>" + innehåll + "</div></li>");
-      
+var highlight_form = document.getElementById('MyRegForm')
+highlight_form.addEventListener('submit', function(event){
     
-    
+    if (document.getElementById('checkbox').checked)
+        {
+            alert("Welcome to Activ8!");
+            return false;
+        }
+    else {
+        event.preventDefault();
+        alert("You have to accept the terms and conditions.");
+        return false;
     }
-  });
-});
+},false );
